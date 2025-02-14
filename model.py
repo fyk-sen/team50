9b7ed2cd700afbee265ea8b7024edaa52f67a948
Bn6666 <150897432+Bn6666@users.noreply.github.com>
2025-02-14 18:41:30 +0800

    Added data to test and change file directory to match output

--- model.py
+++ model.py
@@ -11,7 +11,10 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import mean_squared_error, r2_score
 
 
-DATA_PATH = "/mnt/data/cleaned_data.csv"
+X_TRAIN_DATA_PATH = "/mnt/data/x_train.csv"
+X_TEST_DATA_PATH = "/mnt/data/x_test.csv"
+Y_TRAIN_DATA_PATH = "/mnt/data/y_train.csv"
+Y_TEST_DATA_PATH = "/mnt/data/y_test.csv"
 MODEL_DIR = "/mnt/model_storage/"
 MODEL_PATH = os.path.join(MODEL_DIR, "trained_model.pkl")
 
@@ -19,26 +22,30 @@ MODEL_PATH = os.path.join(MODEL_DIR, "trained_model.pkl")
 os.makedirs(MODEL_DIR, exist_ok=True)
 
 # Set up MLflow tracking
-mlflow.set_tracking_uri("http://mlflow-service:5000")  # Change if MLflow is on a different service
+mlflow.set_tracking_uri("http://mlflow-service:5004")  # Change if MLflow is on a different service
 mlflow.set_experiment("Model_Training")
 
 def train_model():
     """
     function to train model
     """
-    if not os.path.exists(DATA_PATH):
+    if not os.path.exists(X_TRAIN_DATA_PATH):
         print("No Data available")
         return
 
     # Read data
-    data = pd.read_csv(DATA_PATH)
-    x = data.drop("status", axis=1)
-    y = data["status"]
+    x_train = pd.read_csv(X_TRAIN_DATA_PATH)
+    x_test = pd.read_csv(X_TEST_DATA_PATH)
+    y_train = pd.read_csv(Y_TRAIN_DATA_PATH)
+    y_test = pd.read_csv(Y_TEST_DATA_PATH)
+    # data = pd.read_csv(X_TRAIN_DATA_PATH)
+    # x = data.drop("status", axis=1)
+    # y = data["status"]
 
-    stratified_split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-    for train_idx, test_idx in stratified_split.split(x, y):
-        x_train, x_test = x.iloc[train_idx], x.iloc[test_idx]
-        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+    # stratified_split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    # for train_idx, test_idx in stratified_split.split(x, y):
+    #     x_train, x_test = x.iloc[train_idx], x.iloc[test_idx]
+    #     y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
 
     # Parameters
     n_estimators = 100
