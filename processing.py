6b90690f1538e8b57d8fae13411020c313d5a399
fyxen <150899536+fyk-sen@users.noreply.github.com>
2025-02-14 14:41:43 +0800

    export split data

--- processing.py
+++ processing.py
@@ -6,8 +6,12 @@ from sklearn.impute import KNNImputer
 from sklearn.model_selection import StratifiedShuffleSplit
 
 RAW = "/data/raw.csv"
+X_TRAIN = "/data/x_train.csv"
+Y_TRAIN = "/data/y_train.csv"
+X_TEST = "/data/x_test.csv"
+Y_TEST = "/data/y_test.csv"
 
-data = pd.read_csv('raw.csv')
+data = pd.read_csv(RAW)
 df = data
 
 df.drop(columns=['name'], inplace=True)
@@ -30,3 +34,9 @@ y_train = df.loc[train_idx, 'status']
 
 X_test = df.loc[test_idx, feature_cols]
 y_test = df.loc[test_idx, 'status']
+
+X_train.to_csv(X_TRAIN, index=False)
+y_train.to_csv(Y_TRAIN, index=False)
+
+X_test.to_csv(X_TEST, index=False)
+y_test.to_csv(Y_TEST, index=False)
