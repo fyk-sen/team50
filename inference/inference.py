ca52dff3630e50247a062791e764c785c4a0c9f4
Darienlah <150897234+Darienlah@users.noreply.github.com>
2025-02-17 10:54:55 +0800

    Updated to include PVC

--- inference/inference.py
+++ inference/inference.py
@@ -1,10 +1,15 @@
 # model_infer.py
 import joblib
 import numpy as np
+import os
 
-MODEL_PATH = "/model/trained_model.pkl"
+MODEL_PATH = "/model-storage/trained_model.pkl"
 
 def predict(features):
+    if not os.path.exists(MODEL_PATH):
+        print("Model not found. Train the model first.")
+        return None
+
     model = joblib.load(MODEL_PATH)
     prediction = model.predict([features])
     return prediction[0]
@@ -12,4 +17,5 @@ def predict(features):
 if __name__ == "__main__":
     test_input = [2, 5]
     result = predict(test_input)
-    print(f"Prediction for input {test_input}: {result}")
+    if result is not None:
+        print(f"Prediction for input {test_input}: {result}")
