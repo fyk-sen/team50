262f220d83fb3013bc9af29c70b529f8ad7a317f
a13y <alyciayakk@gmail.com>
2025-02-19 11:46:07 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,49 +1,6 @@
 #!/bin/bash
 
 echo "Removing Previous Deployment..."
-minikube stop
-minikube delete
-
-echo "Starting Minikube..."
-minikube start --driver=docker
-
-echo "Checking Minikube Status..."
-kubectl get nodes
-
-echo "Setting Docker Environment..."
-eval $(minikube docker-env)
-
-echo "Building Docker Images..."
-cd application
-docker build -t application .
-cd ../processing
-docker build -t processing .
-
-echo "Checking Docker Images..."
-docker images
-
-echo "Applying Kubernetes Configurations..."
-cd ..
-kubectl apply -f shared-pv.yaml
-kubectl apply -f shared-pvc.yaml
-
-echo "Checking Persistent Volume & Persistent Volume Claims..."
-kubectl get pv
-kubectl get pvc
-
-echo "Applying Kubernetes Configurations..."
-kubectl apply -f application/application.yaml
-kubectl apply -f processing/processing.yaml
-
-echo "Checking Pods..."
-kubectl get pods
-
-echo "Getting Application Service URL..."
-minikube service application-service --url
-
-
-____________________________________________________________________________________________________________________
-
 kubectl delete pods --all
 kubectl delete deployments --all
 kubectl delete services --all
@@ -52,14 +9,18 @@ kubectl delete pv --all
 
 minikube stop
 minikube delete
+
+echo "Starting Minikube..."
 minikube start --driver=docker
 eval $(minikube docker-env)
 
+echo "Building Docker Images..."
 docker build -t model ./model
 docker build -t application ./application
 docker build -t processing ./processing
 docker build -t inference ./inference
 
+echo "Applying Kubernetes Configurations..."
 kubectl apply -f shared-pv.yaml
 kubectl apply -f shared-pvc.yaml
 kubectl apply -f mlflow-pvc.yaml
@@ -71,5 +32,24 @@ kubectl apply -f inference/inference.yaml
 kubectl apply -f model/model.yaml
 kubectl apply -f mlflow.yaml
 
-minikube service application-service --url
-minikube service mlflow-service --url
+echo "Checking Pods Status..."
+# Wait until all pods are Running or Completed
+while true; do
+    STATUS=$(kubectl get pods --no-headers | awk '{print $3}' | grep -vE 'Running|Completed')
+
+    if [ -z "$STATUS" ]; then
+        echo "All pods are Running!"
+        break
+    else
+        echo "Waiting for pods to be ready..."
+        sleep 5
+    fi
+done
+
+# Open application and MLflow service in two different terminals
+
+# Open Application Service
+gnome-terminal -- bash -c "echo 'Opening Application Service...'; xdg-open \$(minikube service application-service --url); exec bash"
+
+# Open MLflow Service
+gnome-terminal -- bash -c "echo 'Opening MLflow Service...'; xdg-open \$(minikube service mlflow-service --url); exec bash"
